7188c55a72309c721dae9b0c11e11c29b1a483d0
OfflineBot <69619904+OfflineBot@users.noreply.github.com>
2024-02-26 17:41:31 +0100

    Update html_template.rs

--- src/convert/html_template.rs
+++ src/convert/html_template.rs
@@ -9,7 +9,7 @@ pub fn html_template(back_route: String, title: String, body: String) -> String 
 <head>
     <meta charset=\"UTF-8\">
     <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">
-    <link rel=\"stylesheet\" href=\"{}/css/styles.css\">
+    <link rel=\"stylesheet\" href=\"{}css/styles.css\">
     <title>{}</title>
 </head>
 <body>
