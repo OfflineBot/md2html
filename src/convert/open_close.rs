482fcdb5e51948b1b0712d593390d9a065e8721e
OfflineBot <FlintStoneLG@gmail.com>
2024-03-10 13:21:21 +0100

    x

--- src/convert/open_close.rs
+++ src/convert/open_close.rs
@@ -2,12 +2,12 @@ use crate::Settings;
 
 
 #[allow(unused)]
-fn replace_item(mut line: String, from: &str, to: &str) -> String {
+fn replace_item(mut line: String, key: &str, from: &str, to: &str) -> String {
 
-    line += from;
+    line += key;
     let mut output = String::new();
 
-    let split = line.split(from).collect::<Vec<&str>>();
+    let split = line.split(key).collect::<Vec<&str>>();
     let mut counter = 0;
     for i in split.iter() {
         if counter % 2 == 0 {
@@ -18,7 +18,7 @@ fn replace_item(mut line: String, from: &str, to: &str) -> String {
             counter += 1;
         }
     }
-    let replacement = format!("<{}></{}>", &to, &to);
+    let replacement = format!("{}{}", &from, &to);
     output = output.replace(replacement.as_str(), "");
     output
 
@@ -33,7 +33,7 @@ pub fn open_close(line: String, settings: &Settings) -> String {
 
     for i in list.iter() {
         if output.contains(&i[0]) {
-            output = replace_item(line.clone(), &i[1], &i[2]);
+            output = replace_item(line.clone(), &i[0], &i[1], &i[2]);
         }
     }     
 
