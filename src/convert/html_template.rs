628e045b3ce0a4ff1348ce7280f998cae8e3860a
OfflineBot <FlintStoneLG@gmail.com>
2024-03-10 13:16:45 +0100

    x

--- src/convert/html_template.rs
+++ src/convert/html_template.rs
@@ -9,7 +9,8 @@ pub fn html_template(back_route: String, title: String, body: String) -> String 
     <meta charset=\"UTF-8\">
     <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">
     <link rel=\"stylesheet\" href=\"{}css/styles.css\">
-    <script src=\"https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.1/MathJax.js?config=TeX-AMS-MML_HTMLorMML\"></script>
+    <script src=\"https://polyfill.io/v3/polyfill.min.js?features=es6\"></script>
+    <script id=\"MathJax-script\" async src=\"https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js\"></script>
     <title>{}</title>
 </head>
 <body>
